b636da8ada1971234519fcd04dd02d3a7d5ce50a
Gabriel Henton <gabehenton@gmail.com>
2020-11-17 16:04:57 -0800

    MVP

--- app/src/App.js
+++ app/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Kitty from './components/kitty';
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className = "app">
+    <h1>The Minister's Cat is a HAPPY Cat...</h1>
+    <Kitty />
     </div>
-  );
-}
+  )
+  }
+
 
 export default App;
